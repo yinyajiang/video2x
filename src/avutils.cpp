6403140f9f9d0f1e478651f515014e636673b9f0
yinyajiang <32933356+yinyajiang@users.noreply.github.com>
2025-08-26 16:42:53 +0800

    up

--- src/avutils.cpp
+++ src/avutils.cpp
@@ -136,7 +136,7 @@ AVPixelFormat get_encoder_default_pix_fmt(const AVCodec* encoder, AVPixelFormat 
     return best_pix_fmt;
 }
 
-[[gnu::target_clones("arch=x86-64-v4", "arch=x86-64-v3", "default")]]
+// [[gnu::target_clones("arch=x86-64-v4", "arch=x86-64-v3", "default")]]
 float get_frame_diff(AVFrame* frame1, AVFrame* frame2) {
     if (!frame1 || !frame2) {
         logger()->error("Invalid frame(s) provided for comparison");
