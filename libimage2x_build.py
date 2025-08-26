2fdaeb93fdea3f298b2bb0169e68daec4268a08b
yinyajiang <32933356+yinyajiang@users.noreply.github.com>
2025-08-26 15:49:34 +0800

    Create libimage2x_build.py

---
+++ libimage2x_build.py
@@ -0,0 +1,40 @@
+import os
+import pathlib
+import sys
+import subprocess
+import platform
+import shutil
+
+cur_dir = pathlib.Path(os.path.dirname(os.path.abspath(__file__)))
+
+def main():
+    args = sys.argv[1:]
+    output_dir = None
+    for i, arg in enumerate(args):
+        if arg == '-o':
+            output_dir = args[i + 1]
+            args.pop(i)
+            args.pop(i)
+            break
+
+    subprocess.run(['python' if platform.system() == 'Windows' else 'python3', cur_dir / 'tools' / 'libimage2x' / 'build.py'] + args).check_returncode()
+    print("build success")
+    if output_dir:
+        if platform.system() == 'Windows':
+            release_dir = cur_dir / 'tools' / 'libimage2x' / 'build' / 'Release'
+            # 拷贝 *.dll 和 *.exe 到 output_dir
+            for file in release_dir.glob('*.dll'):
+                shutil.copy(file, output_dir / file.name)
+            for file in release_dir.glob('*.exe'):
+                shutil.copy(file, output_dir / file.name)
+            print("copy dll and exe to:", output_dir)
+        else:
+            release_dir =  cur_dir / 'tools' / 'libimage2x' / 'build'
+            for file in release_dir.glob('*.dylib'):
+                shutil.copy(file, output_dir / file.name)
+            print("copy dylib to:", output_dir)
+    else:
+        print("no output dir")
+    
+if __name__ == "__main__":
+    main()
